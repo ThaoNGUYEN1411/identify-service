3ffc53a9be34064248058b24094bd9453aafe50d
Thao NGUYEN <phuongthaolyon@gmail.com>
2024-09-10 00:22:23 +0200

    add getUserById and deleteUserById

--- src/main/java/com/thao/identityservice/controller/UserController.java
+++ src/main/java/com/thao/identityservice/controller/UserController.java
@@ -3,11 +3,20 @@ package com.thao.identityservice.controller;
 import com.thao.identityservice.dto.request.UserCreationRequest;
 import com.thao.identityservice.entity.User;
 import com.thao.identityservice.service.UserService;
+import org.springframework.data.repository.query.Param;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+
 @RestController
+@RequestMapping("/users")
 public class UserController {
     private final UserService userService;
 
@@ -15,8 +24,29 @@ public class UserController {
         this.userService = userService;
     }
 
-    @PostMapping("/users")
+    @PostMapping
     User createUser(@RequestBody UserCreationRequest request){
         return userService.createRequest(request);
+    }
+
+    @GetMapping
+    List<User> getUsers(){
+        return userService.getUsers();
+    }
+
+    @GetMapping("/{userId}")
+    User getUserById(@PathVariable("userId") Long userId){
+        return userService.getUserById(userId);
+    }
+
+    @PutMapping("/{userId}")
+    User updateUser(@PathVariable("userId") Long id, @RequestBody UserCreationRequest request){
+        return userService.updateUser(id, request);
+    }
+
+    @DeleteMapping("/{userId}")
+    String deleteUser(@PathVariable Long userId){
+        userService.deleteUser(userId);
+        return "User has been deleted";
     }
 }
--- src/main/java/com/thao/identityservice/service/UserService.java
+++ src/main/java/com/thao/identityservice/service/UserService.java
@@ -6,6 +6,8 @@ import com.thao.identityservice.mapper.UserMapper;
 import com.thao.identityservice.repository.UserRepository;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @Service
 public class UserService {
     private final UserRepository userRepository;
@@ -20,5 +22,30 @@ public class UserService {
         User user = userMapper.toEntity(request);
 
         return userRepository.save(user);
+    }
+
+    public List<User> getUsers() {
+        return userRepository.findAll();
+    }
+
+    public User getUserById(Long id) {
+        return userRepository.findById(id).orElseThrow(() -> new RuntimeException("User not found"));
+    }
+
+    public User updateUser(Long id, UserCreationRequest request) {
+        User user = getUserById(id);
+
+        user.setUsername(request.getUsername());
+        user.setUsername(request.getUsername());
+        user.setLastName(request.getLastName());
+        user.setPassword(request.getPassword());
+        user.setBirthdate(request.getBirthdate());
+
+        return userRepository.save(user);
+
+    }
+
+    public void deleteUser(Long id) {
+        userRepository.deleteById(id);
     }
 }
